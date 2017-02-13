70f0d6257cfc68e433b80f987b4b2996bb42e5f4
giannis <shaihulud77@gmail.com>
2017-02-13 23:52:06 +0100

    main GD

--- week_1/mainGradientDescent.m
+++ week_1/mainGradientDescent.m
@@ -8,16 +8,17 @@ rastrigin = @(x, y) 20 + x.^2 - 10 .* cos(2 .* pi .* x) + y.^2 - 10 .* cos(2 .* 
 rastriginfx = @(x,y) 2*x + 20*pi*sin(2*pi*x);
 rastriginfy = @(x,y) 2*y + 20*pi*sin(2*pi*y);
 
-numExperiments = 100;
+numExperiments = 10;
 scores = zeros(1, numExperiments);
 for i = 1:numExperiments
-    % [x0, y0, score] = runGradientDescent(rosenbrock, rosenfx, rosenfy, 0.00005, 0);
-    [x0, y0, score] = runGradientDescent(rastrigin, rastriginfx, rastriginfy, 0.01, 0);
+    [x0, y0, score] = runGradientDescent(rosenbrock, rosenfx, rosenfy, 0.000001, 0);
+    % [x0, y0, score] = runGradientDescent(rastrigin, rastriginfx, rastriginfy, 0.001, 0);
     scores(i) = score;
 end;
 
 avg_score = sum(scores) / numExperiments;
 sprintf('Average score: %d std_dev: %d ', avg_score, std(scores))
 
+% Can be run for better visualization of the results
 % [x0, y0, score] = runGradientDescent(rosenbrock, rosenfx, rosenfy, 0.00001);
 % [x0, y0, score] = runGradientDescent(rastrigin, rastriginfx, rastriginfy, 0.001);
