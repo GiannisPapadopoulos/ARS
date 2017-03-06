615a1865707904c0aed3328625ce28f1a7bfa16c
giannis <shaihulud77@gmail.com>
2017-03-06 19:45:07 +0100

    main loop

--- assignment2/GA.m
+++ assignment2/GA.m
@@ -1,12 +1,14 @@
 classdef GA
    properties
-        World
-        Individuals;
+       PopulationSize;
+       World;
+       Individuals;
    end 
     
    methods
        
        function obj = GA(initialPopulationSize)
+           obj.PopulationSize = initialPopulationSize;
            obj.World = World();
            obj.Individuals = repmat(Individual(),1,initialPopulationSize);
            for i=1:initialPopulationSize
@@ -17,23 +19,44 @@ classdef GA
        end
        
        function run(obj, numGenerations)
+           avgFitnessPerGeneration = zeros(numGenerations , 1);
            for i = 1:numGenerations
                for j = 1: length(obj.Individuals)
                    % TODO
                    % Run for x steps and calculate fitness
+                   fitness = simulateAndCalculateFitness(obj, obj.Individuals(j));
+                   obj.Individuals(j).Fitness = fitness;
                end
+               avgFitnessPerGeneration(i) = obj.averageFitness();
                fittest = obj.doSelection();
-               newIndividuals = reproduce(fittest);
+               newIndividuals = obj.reproduce(fittest);
                obj.Individuals = newIndividuals;
            end 
+           avgFitnessPerGeneration
+       end
+       
+       function fitness = simulateAndCalculateFitness(obj, individuals)
+           % random initial positions which don't collide with any walls
+           initialPositions = [1 1; 5 2; 10 1.5; 15 2; 18 4; 20 8; 17 9; 10 10.75; 5 10.75; 20 2 ]; 
+           r = randi([1 length(initialPositions)],1,1);
+           randAngleDeg = rand(1) * 360;
+           robot = Robot(initialPositions(r,1), initialPositions(r,2), randAngleDeg);
+           fitness = 0;
+       end
+       
+       function sortByFitness(obj)
+           [~, ind] = sort([obj.Individuals]);
+           obj.Individuals = obj.Individuals(ind);
        end
        
        function selected = doSelection(obj)
            % TODO Return fittest individuals
+           selected = obj.Individuals;
        end
        
-       function newGeneration = reproduce(obj)
+       function newGeneration = reproduce(obj, fittest)
            % TODO Return fittest individuals
+           newGeneration = fittest;
        end
        
        function avgFitness = averageFitness(obj)
--- assignment2/navigation.m
+++ assignment2/navigation.m
@@ -2,9 +2,18 @@ w = World()
 r = Robot(10,2,0)
 
 w.plotWorld();
-r.plotRobot();
+%r.plotRobot();
+
 
 % All sensor distances
 distances = r.getAllSensorDistances(w)
 
 Theta = rand(6,2);
+
+initialPositions = [1 1; 5 2; 10 1.5; 15 2; 18 4; 20 8; 17 9; 10 10.75; 5 10.75; 20 2 ]; 
+
+for i = 1:length(initialPositions)
+    r = Robot(initialPositions(i, 1), initialPositions(i, 2), 0);
+    r.plotRobot();
+end
+
